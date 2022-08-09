ca18cb335b9712265ac1f67b02fc8d395784c9dd
shubhrajyoti.bhattacharjee@zs.com <shubhrajyoti.bhattacharjee@zs.com>
2022-08-09 11:18:27 +0000

    new commit

--- models/customers.sql
+++ models/customers.sql
@@ -53,3 +53,20 @@ final as (
 )
 
 select * from final
+
+select
+order_id,
+{% for payment_method in ["bank_transfer", "credit_card", "gift_card"] %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
+{% endfor %}
+sum(amount) as total_amount
+from {{ ref('raw_payments') }}
+group by 1
+
+
+
+
+
+
+
+
